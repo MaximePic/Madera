22aa78e2f767fb6663f68ea64b61db355f7cab8e
Jim Lry <ler.jimmy@gmail.com>
2017-04-06 11:43:17 +0200

    Migration webpack 1 to 2 + conf scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,41 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/main/js/app.js',
+    entry: ['./src/main/js/app.js', './src/main/resources/static/css/main.scss'],
     devtool: 'sourcemaps',
     cache: true,
-    debug: true,
     output: {
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
                 }
+            },
+            { // regular css files
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    loader: 'css-loader?importLoaders=1',
+                }),
+            },
+            { // sass / scss loader for webpack
+                test: /\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({ // define where to save the file
+            filename: './src/main/resources/static/built/bundle.css',
+            allChunks: true,
+        }),
+    ],
 };
